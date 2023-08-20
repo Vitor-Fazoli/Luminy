2f976f4474222596670e047bf0654f6c295d13ac
Fazoli <86382456+Vitor-Fazoli@users.noreply.github.com>
2023-08-20 20:20:28 -0300

    Add files via upload

---
+++ Lumixy.py
@@ -0,0 +1,60 @@
+#these are informations about your addon
+bl_info = {
+    "name": "Lumixy",
+    "blender": (3, 6, 2),
+    "category": "Object",
+}
+
+#this is a place to begin a code
+
+# for now it's only STUDIES, anything like that not represents my add-on
+
+import bpy
+from bpy.app.handlers import persistent
+
+@persistent
+def load_handler_for_preferences(_):
+    print("Changing Preference Defaults!")
+    from bpy import context
+
+    prefs = context.preferences
+    prefs.use_preferences_save = False
+
+    kc = context.window_manager.keyconfigs["blender"]
+    kc_prefs = kc.preferences
+    if kc_prefs is not None:
+        kc_prefs.select_mouse = 'RIGHT'
+        kc_prefs.spacebar_action = 'SEARCH'
+        kc_prefs.use_pie_click_drag = True
+
+    view = prefs.view
+    view.header_align = 'BOTTOM'
+
+
+@persistent
+def load_handler_for_startup(_):
+    print("Changing Startup Defaults!")
+
+    # Use smooth faces.
+    for mesh in bpy.data.meshes:
+        for poly in mesh.polygons:
+            poly.use_smooth = True
+
+    # Use material preview shading.
+    for screen in bpy.data.screens:
+        for area in screen.areas:
+            for space in area.spaces:
+                if space.type == 'VIEW_3D':
+                    space.shading.type = 'MATERIAL'
+                    space.shading.use_scene_lights = True
+
+
+def register():
+    print("Registering to Change Defaults")
+    bpy.app.handlers.load_factory_preferences_post.append(load_handler_for_preferences)
+    bpy.app.handlers.load_factory_startup_post.append(load_handler_for_startup)
+
+def unregister():
+    print("Unregistering to Change Defaults")
+    bpy.app.handlers.load_factory_preferences_post.remove(load_handler_for_preferences)
+    bpy.app.handlers.load_factory_startup_post.remove(load_handler_for_startup)
