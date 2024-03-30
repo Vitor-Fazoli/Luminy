b72e5abc4233beb0edb9c76107eb4cf17b1477d9
Fazoli <86382456+Vitor-Fazoli@users.noreply.github.com>
2024-03-30 17:59:42 -0300

    atualização para melhora da analíse

--- Lumixy.py
+++ Lumixy.py
@@ -1,9 +1,15 @@
-from asyncore import read
+import random
+
 import numpy as np
 import torch
 import torchvision
 import matplotlib.pyplot as plt
 import cv2
+from openpyxl import load_workbook
+import psutil
+import time
+from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+
 
 def show_anns(anns):
     if len(anns) == 0:
@@ -11,51 +17,53 @@ def show_anns(anns):
     sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
     ax = plt.gca()
     ax.set_autoscale_on(False)
-    polygons = []
-    color = []
     for ann in sorted_anns:
         m = ann['segmentation']
         img = np.ones((m.shape[0], m.shape[1], 3))
         color_mask = np.random.random((1, 3)).tolist()[0]
-        for i in range(3):
-            img[:,:,i] = color_mask[i]
-        ax.imshow(np.dstack((img, m*0.35)))
+        for n in range(3):
+            img[:, :, n] = color_mask[n]
+        ax.imshow(np.dstack((img, m * 0.35)))
+
+
+
+print("Lumixy version: 0.0.3\n")
 
 print("PyTorch version:", torch.__version__)
 print("Torchvision version:", torchvision.__version__)
-print("CUDA is available:", torch.cuda.is_available())
-print(torch.version.cuda)
 
-
-
-image = cv2.imread('C:/Users/vhfaz/source/repos/Lumixy/Lumixy/Assets/image.png')
+image = cv2.imread('Assets/example_128x128.png')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-#plt.figure(figsize=(10,10))
-#plt.imshow(image)
-#plt.axis('off')
-#plt.show()
+initial_time = time.time()
+useMemoryInitial = str(psutil.virtual_memory().used / (1024 * 1024 * 1024)) + 'GB'
+useCpuInitial = str(psutil.cpu_percent().real) + ' %'
 
-
-import sys
-sys.path.append("..")
-from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
-
-sam_checkpoint = "C:/Users/vhfaz/source/repos/Lumixy/Lumixy/Assets/sam_vit_h_4b8939.pth"
-model_type = "vit_h"
-
-device = "cuda"
-
-sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-sam.to(device=device)
-
+print('Processo iniciado!')
+sam = sam_model_registry["vit_h"](checkpoint="Assets/sam_vit_h_4b8939.pth")
 mask_generator = SamAutomaticMaskGenerator(sam)
 masks = mask_generator.generate(image)
 
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(10, 10))
 plt.imshow(image)
+identity = str(random.randrange(10000000))
 show_anns(masks)
 plt.axis('off')
-plt.show() 
+plt.savefig('Assets/Results/' + str(identity) + '.png')
 
-read()
+useMemoryFinal = str(psutil.virtual_memory().used / (1024 * 1024 * 1024)) + 'GB'
+useCpuFinal = str(psutil.cpu_percent().real) + ' %'
+
+final_time = time.time()
+total_time = final_time - initial_time
+
+wb = load_workbook('dados.xlsx')
+
+sheet = wb['principal']
+
+new_row = [useMemoryInitial, useMemoryFinal, useCpuInitial, useCpuFinal, image.size, total_time, str(identity) + '.png']
+sheet.append(new_row)
+
+wb.save('dados.xlsx')
+
+print('Processo encerrado!')
