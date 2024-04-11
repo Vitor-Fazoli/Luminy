4e293442affefec42c8450b3f980402ba906d36c
Fazoli <86382456+Vitor-Fazoli@users.noreply.github.com>
2024-04-10 22:51:42 -0300

    Add files via upload

--- Luminy.py
+++ Luminy.py
@@ -16,21 +16,56 @@ def s_generate_identity():
 def v_generate_mask(mask):
     if len(mask) == 0:
         return
+
+    # Cores padrão
+    pattern_colors = [
+        [1, 0, 0],  # Vermelho
+        [0, 1, 0],  # Verde
+        [0, 0, 1],  # Azul
+        [1, 1, 0],  # Amarelo
+        [1, 0, 1],  # Magenta
+    ]
+
+    # Inicializa o índice da cor
+    index_color = 0
+
     sorted_ans = sorted(mask, key=(lambda x: x['area']), reverse=True)
     ax = plt.gca()
     ax.set_autoscale_on(False)
-    for ann in sorted_ans:
+
+    color_labels = []  # Lista para armazenar as cores de cada pixel
+
+    for ann in mask:
         m = ann['segmentation']
         img = np.ones((m.shape[0], m.shape[1], 3))
-        color_mask = np.random.random((1, 3)).tolist()[0]
-        for n in range(3):
-            img[:, :, n] = color_mask[n]
-        ax.imshow(np.dstack((img, m * 0.35)))
+
+        # Mantém as áreas pretas da imagem original
+        img[np.where(m == 0)] = [0, 0, 0]
+
+        # Aplica a cor padrão apenas às áreas não pretas
+        color_mask = pattern_colors[index_color]
+        img[np.where(m != 0)] = color_mask
+
+        index_color = (index_color + 1) % len(pattern_colors)
+
+        ax.imshow(np.dstack((img, m)))
+
+        # Adiciona as cores dos pixels à lista
+        for y in range(m.shape[0]):
+            for x in range(m.shape[1]):
+                if m[y, x] != 0:
+                    color_labels.append(tuple(img[y, x]))
+
+    print(color_labels)
 
 
+
+
+sam = sam_model_registry["vit_h"](checkpoint="Assets/sam_vit_h_4b8939.pth")
+mask_generator = SamAutomaticMaskGenerator(sam)
 Config.start_program()
 
-image = cv2.imread('Assets/Images/example_16x16.png')
+image = cv2.imread('Assets/Images/teste.png')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 initial_time = time.time()
@@ -38,16 +73,18 @@ useMemoryInitial = str(psutil.virtual_memory().used / (1024 * 1024 * 1024)) + ' 
 useCpuInitial = str(psutil.cpu_percent().real) + ' %'
 
 print('Program starting')
-sam = sam_model_registry["vit_h"](checkpoint="Assets/sam_vit_h_4b8939.pth")
-mask_generator = SamAutomaticMaskGenerator(sam)
+
 masks = mask_generator.generate(image)
+print("Mask generator finished!")
 
 plt.figure(figsize=(10, 10))
 plt.imshow(image)
 identity = s_generate_identity()
 v_generate_mask(masks)
 plt.axis('off')
-plt.savefig('Assets/Results/Example16x16/' + str(identity) + '.png')
+plt.show()
+
+#plt.savefig('Assets/Results/Example16x16/' + str(identity) + '.png')
 
 useMemoryFinal = str(psutil.virtual_memory().used / (1024 * 1024 * 1024)) + 'GB'
 useCpuFinal = str(psutil.cpu_percent().real) + ' %'
@@ -60,9 +97,9 @@ wb = load_workbook('dados.xlsx')
 sheet = wb['principal']
 
 new_row = [useMemoryInitial, useMemoryFinal, useCpuInitial, useCpuFinal, image.size, total_time, str(identity) + '.png']
-sheet.append(new_row)
+#sheet.append(new_row)
 
-wb.save('dados.xlsx')
+#wb.save('dados.xlsx')
 
 # TODO: Criar uma forma de identificar as mascaras do resultado com o esperado para gerar uma precisão
 
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-<h1 align="center">Luminy</h1>
-<p align="center" width="100px" ><img  src="Icon.svg"/></p>
+<h1 align="center">Lumixy</h1>
 
 <p>Este é um TCC focado em desenvolvimento que tem como ideal trazer o ambiente de iluminação para objetos e paisagens bidimensionais
 Trabalhos Similares (Relight): https://clipdrop.co/relight</p>
@@ -7,23 +6,3 @@ Trabalhos Similares (Relight): https://clipdrop.co/relight</p>
  <p> O trabalho será feito em python e irá recuperar uma imagem em formato png e uma posição de luz dentro da imagem para realizar uma segmentação, detecção de profundidade de cada uma dos grupos segmentados, para por fim executar uma série de calculos sobre cada pixel, para iluminar a imagem de maneira consisa e coesa</p>
 
 <p>30/03/2024 - Hoje realizei a criação de funções para geração de um documento xlsx e imagens para criação dos dados futuros</p>
-
-
-Indicador de comparação objetivo
-
-SSIM -  STRUCTURE SIMILARITY 
-MSE 
-
---------------------------------
-
-Base de imagens
-
-Quantidade de imagens - 500 até 1000
-
-se não encontrar base de imagens
-
-quantidade de imagens 10 pelo menos
-
---------------------------------
-
-Fazer comparação utilizando padrões de cor
