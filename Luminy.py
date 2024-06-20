b56add872782ef153c5659008162261637d80192
Vy_Fazoli <vhfazoli@gmail.com>
2024-06-20 19:35:38 -0300

    finalizado

--- Luminy.py
+++ Luminy.py
@@ -1,6 +1,23 @@
+import os
 import numpy as np
 import cv2
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+from openpyxl import load_workbook
+import skimage as ski
+
+
+def save_data(data, excel_file):
+    workbook = load_workbook(filename=excel_file)
+
+    sheet = workbook.active
+    indicate_column = 'A'
+    next_line = 1
+    while sheet[f"{indicate_column}{next_line}"].value is not None:
+        next_line += 1
+
+    sheet.append(*[data])
+
+    workbook.save(excel_file)
 
 
 def generate_mask(anns):
@@ -11,6 +28,9 @@ def generate_mask(anns):
 
     final_image = np.zeros((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 3),
                            dtype=np.uint8)
+
+    # Esse padrão de cores não tem nenhuma interferencia verdadeira,
+    # pois na analíse ele recebe a referencia da imagem e recebe um match de cores
 
     pattern_colors = [
         [255, 0, 0],  # Vermelho
@@ -23,6 +43,7 @@ def generate_mask(anns):
         [50, 50, 50]  # cinza
     ]
 
+    #
     for idx, ann in enumerate(sorted_anns):
         m = ann['segmentation']
         color_mask = pattern_colors[idx % len(pattern_colors)]
@@ -35,47 +56,47 @@ def generate_mask(anns):
 
 
 def main():
-    #TODO: Criar o excel para receber as informações
+    files = os.listdir('Assets/Expected')
 
-    #TODO: Ler todos os arquivos dentro da pasta images e armazenar o caminho deles para usar em seguida
-    #TODO: ver a quantidade de arquivos dentro da pastas images
+    for image_path in files:
+        sam = sam_model_registry["vit_h"](checkpoint="Assets/sam_vit_h_4b8939.pth")
+        mask_generator = SamAutomaticMaskGenerator(sam)
 
-    #TODO: Fazer um for passando a quantidade de arquivos
-    sam = sam_model_registry["vit_h"](checkpoint="Assets/sam_vit_h_4b8939.pth")
-    mask_generator = SamAutomaticMaskGenerator(sam)
+        image = cv2.imread('Assets/Images/' + image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # TODO: receber o string de caminho das imagens
-    image = cv2.imread('Assets/Images/example_16x16.png')
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        masks = mask_generator.generate(image)
+        founded_image = generate_mask(masks)
 
-    print('Program starting')
+        expected_image = cv2.imread('Assets/Expected/' + image_path)
+        expected_image = cv2.cvtColor(expected_image, cv2.COLOR_BGR2RGB)
 
-    masks = mask_generator.generate(image)
-    founded_image = generate_mask(masks)
+        # MSE Normalized
+        mse = ski.metrics.normalized_root_mse(image, expected_image)
 
-    #TODO: receber o string de caminho das imagens
-    expected_image = cv2.imread("/content/Expected/example_16x16.png")
-    expected_image = cv2.cvtColor(expected_image, cv2.COLOR_BGR2RGB)
-    color_expected = expected_image[0, 0]
-    color_founded = founded_image[0, 0]
+        # Squared Error
+        squared_error = ski.metrics.mean_squared_error(image, expected_image)
 
-    error_count = 0
+        color_expected = expected_image[0, 0]
+        color_founded = founded_image[0, 0]
 
-    for y in range(founded_image.shape[1]):
-        for x in range(founded_image.shape[0]):
-            if not np.array_equal(expected_image[x, y], color_expected):
-                color_expected = expected_image[x, y]
-                color_founded = founded_image[x, y]
+        error_count = 0
 
-            if np.array_equal(founded_image[x, y], color_founded):
-                continue
-            else:
-                error_count += 1
+        for y in range(founded_image.shape[1]):
+            for x in range(founded_image.shape[0]):
+                if not np.array_equal(expected_image[x, y], color_expected):
+                    color_expected = expected_image[x, y]
+                    color_founded = founded_image[x, y]
 
-    pixels_count = expected_image.shape[0] * expected_image.shape[1]
-    error_percentage = abs((error_count / pixels_count) * 100 - 100)
+                if np.array_equal(founded_image[x, y], color_founded):
+                    continue
+                else:
+                    error_count += 1
 
-    #TODO: salvar as informações na tabela Results.xlsx
+        pixels_count = expected_image.shape[0] * expected_image.shape[1]
+        error_percentage = abs((error_count / pixels_count) * 100 - 100)
+        save_data([image_path, pixels_count, error_percentage, mse, squared_error], 'dados.xlsx')
+
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Trabalhos Similares (Relight): https://clipdrop.co/relight</p>
  <p> O trabalho será feito em python e irá recuperar uma imagem em formato png e uma posição de luz dentro da imagem para realizar uma segmentação, detecção de profundidade de cada uma dos grupos segmentados, para por fim executar uma série de calculos sobre cada pixel, para iluminar a imagem de maneira consisa e coesa</p>
 
 <p>30/03/2024 - Hoje realizei a criação de funções para geração de um documento xlsx e imagens para criação dos dados futuros</p>
+
+<p>05/06/2024 - Para fazer as analíses aprimoradas das pixel arts, estou usando a biblioteca skimage -> mas encontrei um problema referente ao uso da metrica Similarity, pois não é possível fazer com imagens menores que 7x7 e em ambito das pixel artes existem várias utilizadas que em pelo menos um dos lados tem menos de 7 pixels, oque torna essa metrica impossivel de ser usada </p>
