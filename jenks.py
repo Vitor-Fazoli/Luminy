e5102612cdeb98e7c3fe6d0ee5195a5e2ced5939
Vy_Fazoli <vhfazoli@gmail.com>
2024-07-05 14:09:52 -0300

    898

---
+++ jenks.py
@@ -0,0 +1,20 @@
+
+
+from openpyxl import load_workbook
+from jenkspy import JenksNaturalBreaks
+
+
+workbook = load_workbook(filename='dados.xlsx')
+sheet = workbook.active
+indicate_column = 'B'
+next_line = 1
+
+lista = []
+
+
+while sheet[f"{indicate_column}{next_line}"].value is not None:
+    lista.add(sheet[f"{indicate_column}{next_line}"].value)
+    next_line += 1
+
+
+workbook.save('dados.xlsx')
