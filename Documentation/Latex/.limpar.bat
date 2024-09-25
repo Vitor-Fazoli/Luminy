b99ba8941d8934841893e1ecd989b3daa4c1935d
Vitor-Fazoli <vhfazoli@gmail.com>
2024-09-24 21:54:33 -0300

    Procedimentos e Técnicas

---
+++ Documentation/Latex/.limpar.bat
@@ -0,0 +1,38 @@
+@echo off
+
+rem Limpeza.
+rem Desenvolvido por David Buzatto
+
+del *.aux
+del *.bak
+del *.bbl
+del *.blg
+del *.brf
+del *.def
+del *.equ
+del *.exp
+del *.gz
+del *.gz(busy)
+del *.idx
+del *.ilg
+del *.lis
+del *.listing
+del *.lof
+del *.log
+del *.loq
+del *.lot
+del *.nav
+del *.out
+del *.pdf
+del *.pyg
+del *.sav
+del *.sigla
+del *.siglax
+del *.snm
+del *.stf
+del *.symbols
+del *.symbolsx
+del *.synctex
+del *.synctex(busy)
+del *.toc
+del *.xwm
